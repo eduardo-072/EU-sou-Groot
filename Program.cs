8ec6b3edf3d5b5f04bf616da0beabdff467959aa
eduardo-072 <eduardosilvaaa072@gmail.com>
2023-10-28 02:16:47 -0300

    programa não fecha sem CTRL C

--- Program.cs
+++ Program.cs
@@ -1,71 +1,39 @@
-﻿const string saida = "Adeus";
-
-Console.WriteLine("-----CHAT DO GROOT-----\n");
-
-string pergunta;
-
-do
+﻿internal class Program
 {
-    Console.Write("Faça uma pergunta para o Groot.....:");
-    pergunta = Console.ReadLine()!;
-    
-    string resposta = RespostadoGroot(pergunta);
-    ExibeResposta(RespostadoGroot(pergunta));
-    
-} 
-while  (pergunta.Trim().ToLower() != saida);
-
-    void ExibeResposta(string resposta)
+    private static void Main(string[] args)
     {
-    
-        Console.ForegroundColor = ConsoleColor.DarkYellow;
-        Console.WriteLine($"RESPOSTA {resposta}");
-        Console.ResetColor();      
+        const string saida = "Adeus";
+
+        Console.WriteLine("-----CHAT DO GROOT-----\n");
+
+        string pergunta;
+        do
+        {
+            Console.Write("Fale oi para o Groot.....:");
+            pergunta = Console.ReadLine()!;
+
+            string resposta = RespostadoGroot(pergunta);
+            ExibeResposta(RespostadoGroot(pergunta));
+
+        }
+        while (pergunta.Trim().ToLower() != saida);
+
+        void ExibeResposta(string resposta)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Green
+        ;
+            Console.WriteLine($"RESPOSTA.....: {resposta}");
+            Console.ResetColor();
+        }
+
+        string RespostadoGroot(string pergunta)
+        
+        {
+            return pergunta.Trim().ToLower() == saida ?
+                "Eu sou Groot!" :
+                "Eu sou Groot";
+        }
+        while (saida != "S" && saida != Console.ReadLine()) ;
     }
-  RespostadoGroot(string pergunta);
- {
-  return pergunta.Trim().ToLower() == saida ?
-
-    "Eu sou o Groot!":
-    "Eu sou o Groot";  
- }   
-
-
-
-
-
-
-
-
-
-
-// ﻿const string FraseParaSair = "tchau";
-
-// Console.WriteLine("--- Groot Chatbot ---\n");
-
-// string pergunta;
-// do
-// {
-//     Console.Write("Pergunta: ");
-//     pergunta = Console.ReadLine()!;
-
-//     string resposta = ObtemRespostaGroot(pergunta);
-
-//     ExibeResposta(ObtemRespostaGroot(pergunta));
-
-// } while (pergunta.Trim().ToLower() != FraseParaSair);
-
-
-// void ExibeResposta(string resposta)
-// {
-//     Console.ForegroundColor = ConsoleColor.Green;
-//     Console.WriteLine($"                          Resposta: {resposta}");
-//     Console.ResetColor();
-// }
-
-// string ObtemRespostaGroot(string pergunta)
-// {
-//     return pergunta.Trim().ToLower() == FraseParaSair ?
-//         "Eu sou Groot!" :
-//         "Eu sou Groot.";
-// }
+}
